9def6d0f9e505b58273512aa99c9267f76ba277c
Raunak Priya <raunakpriya@gmail.com>
2023-03-02 15:43:48 +0530

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Harsh Raj on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
